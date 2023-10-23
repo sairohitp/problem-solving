ff6649f08cc48d07471316d5d301567b9e041f1a
Sai Rohit <sairohitp007@gmail.com>
2023-10-23 13:02:25 +0530

    “checkpoint”

---
+++ Python/strings.py
@@ -0,0 +1,40 @@
+"""
+Strings are simply plain text.
+We need to use quotation marks to use them
+"""
+
+print("Giraffe Academy")
+
+# Prints on a new line, i.e escape character
+print("Giraffe\nAcademy")
+print("Giraffe \"Academy\"")
+print("Giraffe\Academy")
+
+phrase = "Giraffe Academy"
+print(phrase)
+
+# Concatenation
+# The process of taking a string and appending another string to it
+print(phrase + " is cool")
+
+# String methods
+# Capitalisation
+print(phrase.lower())
+print(phrase.islower())
+
+print(phrase.upper())
+print(phrase.isupper())
+
+# Combination of methods
+print(phrase.upper().isupper())
+
+# Length of a string
+print(len(phrase))
+
+# We can access a given charecter in a string using []
+
+ourname = "TS Corp"
+# index =  0123456
+
+print(len(ourname)) # its 7 since its starting from 0
+print(ourname[0:2]) # Strign slicing, index 2 is ignored, at 0 and 1 we have T and S so O/P >> TS
--- Python/variables-and-datatypes.py
+++ Python/variables-and-datatypes.py
@@ -1,14 +1,24 @@
 # variables
-
 name = "Sai Rohit"
 
 print("Hello " + name + "!")
 
 # example
 character_name = "John"
-character_age = "35"
+character_age = 35
 print("There once was a man named " + character_name + ", ")
 print("he was 35 years old. ")
+
+# we can change the value of the variable half way through the story as well
+character_name = "Mike"
 print("He really liked the name John, ")
 print("but didn't like being 35.")
 
+# data type of character_name: String
+# data type of character_age: Integer
+
+# Boolean
+isMale = True # or False
+
+# Float
+decimal = 50.1234567890
