55292769f5bcb90a16cbe908d56ce51a93afb4f0
Sai Rohit <sairohitp007@gmail.com>
2023-10-23 12:41:54 +0530

    “commit”

---
+++ Python/index.py
@@ -0,0 +1,14 @@
+# variables
+
+name = "Sai Rohit"
+
+print("Hello " + name + "!")
+
+# example
+character_name = "John"
+character_age = "35"
+print("There once was a man named " + character_name + ", ")
+print("he was 35 years old. ")
+print("He really liked the name John, ")
+print("but didn't like being 35.")
+
