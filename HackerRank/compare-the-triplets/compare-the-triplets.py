555c3842d48f4bb9f776e6b3e3c4da15906cfd49
Sai Rohit <sairohitp007@gmail.com>
2023-10-23 16:21:32 +0530

    “checkpoint”

---
+++ HackerRank/compare-the-triplets/compare-the-triplets.py
@@ -0,0 +1,12 @@
+def compareTriplets(a, b):
+    alice = bob = 0
+    
+    for i in range(3):
+        if a[i] > b[i]:
+            alice+=1
+        elif a[i] < b[i]:
+            bob+=1
+    
+    return alice, bob
+
+compareTriplets()
